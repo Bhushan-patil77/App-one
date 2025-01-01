3829561888680a2e234b5d15e569bf69de2f5503
Bhushan-patil77 <145292155+Bhushan-patil77@users.noreply.github.com>
2025-01-02 01:46:28 +0530

    resolved home path try 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,8 +8,8 @@ import useAuthStore from './store/useAuthStore'
 import { Route, Routes, useNavigate } from 'react-router-dom'
 import Login from './components/Login.jsx'
 import Signup from './components/Signup.jsx'
-import Home from './components/home.jsx'
 import Profile from './components/Profile.jsx'
+import Home from './components/h.jsx'
 
 
 function App() {
